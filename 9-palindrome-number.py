968a6db446e45dcedb65f3c6525700ddb9545c26
saddamhr <saddambubt65@gmail.com>
2022-04-24 13:40:36 +0600

    palindrome number solved

---
+++ 9-palindrome-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        string_x = str(x)
+        if string_x == string_x[::-1]:
+            return True
+        else:
+            return False
+        
