5305186af3745d77ce4cb03978f79e991c4153c3
saddamhr <saddambubt65@gmail.com>
2022-04-24 13:39:27 +0600

    two sum solved

---
+++ 1-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dictionary = {}
+        for i in range(len(nums)):
+            difference = target - nums[i]
+            if dictionary.get(difference) is not None:
+                return [dictionary.get(difference), i]
+            else:
+                dictionary[nums[i]] = i
