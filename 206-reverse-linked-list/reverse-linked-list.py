119bbb9642b04ca1c278d5be297783aa13a33a09
“Md_Saddam_Hossain” <saddam.hossain@selise.ch>
2022-08-29 09:42:15 +0600

    .

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -3,18 +3,17 @@
     Time Complexity: 0(n)
     Space Complexity: 0(1)
 '''
-from typing import List
+from typing import Optional
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        left, right, max_profit = 0, 1, 0 # 5
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
 
-        while right < len(prices):
-            if prices[left] < prices[right]: #1<4
-                max_profit = max(max_profit, prices[right]-prices[left])
-            else:
-                left = right
-            right+=1
-        return max_profit
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
 
-print(Solution().maxProfit([7,1,5,3,6,4]))
+print(Solution().reverseList([7,1,5,3,6,4]))
