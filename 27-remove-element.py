24bab8c789bdae7542aa022a46c7ec9dff4666b5
“Md_Saddam_Hossain” <saddam.hossain@selise.ch>
2022-09-04 11:38:32 +0600

    .

---
+++ 27-remove-element.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] != val:
+                nums[left] = nums[right]
+                left+=1
+        return left
+        
+s = Solution()
+print(s.removeElement([3,2,2,3], 3))
