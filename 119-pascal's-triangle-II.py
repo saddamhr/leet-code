d5797ecd83efa5772ab12324cb33c67d7b6c3c76
Md_Saddam_Hossain <saddam.hossain@selise.ch>
2023-03-15 10:38:13 +0600

    Add code to generate Pascal's Triangle

---
+++ 119-pascal's-triangle-II.py
@@ -0,0 +1,22 @@
+from typing import List
+class Solution:
+    def generate(self, rowIndex: int) -> List[List[int]]:
+        result = []
+        for i in range(1, rowIndex+2):
+            subArr = []
+            sum = 0
+            for j in range(1, i+1):
+                if i <= 2:
+                    subArr.append(1)
+                else:
+                    if j == 1:
+                        subArr.append(1)
+                    elif i == j:
+                        subArr.append(1)
+                    else:
+                        val = result[i-2][j-1] + result[i-2][j-2]
+                        subArr.append(val)
+            result.append(subArr)
+        return result[len(result)-1]
+
+print(Solution().generate(3))
