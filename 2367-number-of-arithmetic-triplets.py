674de99fac0c3c814096cdd762f66ff56ce7b22a
Md_Saddam_Hossain <saddam.hossain@selise.ch>
2023-03-19 06:15:56 +0600

    Add two pointer approach to solve 2367-number-of-arithmetic-triplets.py and 39-combination-sum.py

--- 2367-number-of-arithmetic-triplets.py
+++ 2367-number-of-arithmetic-triplets.py
@@ -1,3 +1,4 @@
+from typing import List
 '''
 Brute force approach
 Time complexity: O(n^3)
@@ -17,3 +18,29 @@ class Solution:
     
 # print(Solution().arithmeticTriplets([4,5,6,7,8,9], 2))
 print(Solution().arithmeticTriplets([4,5,6,7,8,9], 2))
+
+
+'''
+Two pointer approach
+Time Complexity: O(n^2)
+Space Complexity: O(n)
+Status: Accepted
+'''
+
+class Solution:
+    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        triplet = 0
+        for i in range(len(nums)):
+            left, right = i + 1, len(nums) - 1
+
+            while left < right:
+                if nums[i] < nums[left] < nums[right] and nums[left] - nums[i] == diff  and nums[right] - nums[left] == diff:
+                    triplet += 1
+                    left+=1
+                elif nums[right] - nums[left] > diff and nums[right] > nums[left]:
+                    right-=1
+                else:
+                    left+=1
+        return triplet
+    
+print(Solution().arithmeticTriplets([0,1,4,6,7,10], 3))
